dd8462de8c31233e4807c174b1f061194e1dc813
Dempsey Palacio Tascon <dempsey.2001@hotmail.co.uk>
2022-10-24 18:47:43 +0100

    CountAndSay_38 solution Completed

--- CountAndSay_38.java
+++ CountAndSay_38.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 /**
  * CountAndSay_38
@@ -8,57 +11,40 @@ public class CountAndSay_38 {
 
     public static void main(String[] args) {
         CountAndSay_38 result = new CountAndSay_38();
-        System.out.println(result.countAndSay(4));
+        System.out.println(result.countAndSay(20));
         
     }
 
     public String countAndSay(int n) {
-        return countAndSayRecursive(n)+"";
+        return countAndSayRecursive(n+"");
     }
 
-    public long countAndSayRecursive(long n) {
-        if(n == 1){
-            return 1;
+    public String countAndSayRecursive(String n) {
+        if(n.equals("1")){
+            return "1";
         } else{
-            return digitCovert(countAndSayRecursive(n-1));
+            int tempN = Integer.parseInt(n)-1;
+            return digitCovertString(countAndSayRecursive(tempN+""));
         }
     }
-    public long digitCovert(long n){
-        int[] intArray = integerToIntArray(n);
-        ArrayList<List<Integer>> pairs = new ArrayList<>();
-        int currrentInt = intArray[0];
-        int numOfCurrentInt = 0;
-        List<Integer> pair = new ArrayList<>();
+
+    public String digitCovertString(String n){
+        String num = "";
+        String[] intArray = n.split("");
+        String currentInt = intArray[0];
+        Integer numOfCurrentInt = 0;
         for(int i =0; i< intArray.length; i++){
-            if(intArray[i] == currrentInt){
+            if(intArray[i].equals(currentInt)){
                 numOfCurrentInt+=1;
             } else{
-                pair.add(numOfCurrentInt);
-                pair.add(currrentInt);
-                pairs.add(pair);
-                pair = new ArrayList<>();
-                currrentInt = intArray[i];
+                num += numOfCurrentInt;
+                num += currentInt;
+                currentInt = intArray[i];
                 numOfCurrentInt = 1;
             }
         }
-        pair.add(numOfCurrentInt);
-        pair.add(currrentInt);
-        pairs.add(pair);
-        String num = "";
-        for(int i = 0; i< pairs.size(); i++){
-            num += pairs.get(i).get(0);
-            num += pairs.get(i).get(1);
-        }
-        return Long.parseLong(num);
-    }
-
-    public int[] integerToIntArray(long n){
-        String temp = Long.toString(n);
-        int[] newGuess = new int[temp.length()];
-        for (int i = 0; i < temp.length(); i++)
-        {
-            newGuess[i] = temp.charAt(i) - '0';
-        }
-        return newGuess;
+        num += numOfCurrentInt;
+        num += currentInt;
+        return num;
     }
 }
