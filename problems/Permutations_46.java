061bebb272d2ecffbd40eb17259e46270ee2ece6
Dempsey Palacio Tascon <dempsey.2001@hotmail.co.uk>
2022-10-26 23:48:51 +0100

    Started Problem 46 Permutations

---
+++ problems/Permutations_46.java
@@ -0,0 +1,17 @@
+package problems;
+
+import java.util.List;
+/**
+ * Permutations_46
+ */
+public class Permutations_46 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        return null;
+        
+    }
+}
